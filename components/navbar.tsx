94e43178c24d0a2fdde68bb14b5bd6ebeb23b908
DivMohammed <dev.mohammed.omar0@gmail.com>
2025-07-13 13:37:23 +0300

    change env

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -32,7 +32,7 @@ export const Navbar = async () => {
   return (
     <div className="border-b">
             <Container>
-                <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between">
+                <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between m-2">
                     <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
                         {/* <Icons.Logo/> */}
                     </Link>
